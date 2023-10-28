da54f80530af489520a5e266e58f03bc44a1e348
lxnd <lxnd@protonmail.com>
2023-10-28 10:22:56 +0200

    refactor: introduce parametrized test for execute commands

--- tests.py
+++ tests.py
@@ -21,7 +21,10 @@ def test_landing(position):
     assert Rover.at(position).position() == position
 
 
-def test_execute_commands():
-    rover = Rover.at("0 0 N")
-    rover.executes("L")
-    assert rover.position() == "0 0 W"
+@pytest.mark.parametrize("initial,commands,final", [
+    ("0 0 N", "L", "0 0 W")
+])
+def test_execute_commands(initial, commands, final):
+    rover = Rover.at(initial)
+    rover.executes(commands)
+    assert rover.position() == final

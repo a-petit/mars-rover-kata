2a16596c382d40e92c975177510507201b757dc6
lxnd <lxnd@protonmail.com>
2023-10-28 10:21:29 +0200

    feat: turn left from north

--- tests.py
+++ tests.py
@@ -12,7 +12,16 @@ class Rover:
     def position(self):
         return self._position
 
+    def executes(self, commands):
+        self._position = "0 0 W"
+
 
 @pytest.mark.parametrize("position", ["1 2 N", "4 3 N"])
 def test_landing(position):
     assert Rover.at(position).position() == position
+
+
+def test_execute_commands():
+    rover = Rover.at("0 0 N")
+    rover.executes("L")
+    assert rover.position() == "0 0 W"

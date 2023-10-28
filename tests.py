33ca172940a8dabf5a7e6d6d8ac5661f84d8ade0
lxnd <lxnd@protonmail.com>
2023-10-28 10:19:08 +0200

    feat: landing

--- tests.py
+++ tests.py
@@ -2,14 +2,17 @@ import pytest
 
 
 class Rover:
+    def __init__(self, position):
+        self._position = position
+
     @classmethod
     def at(cls, position):
-        return Rover()
+        return Rover(position)
 
     def position(self):
-        return "1 2 N"
+        return self._position
 
 
-@pytest.mark.parametrize("position", ["1 2 N"])
+@pytest.mark.parametrize("position", ["1 2 N", "4 3 N"])
 def test_landing(position):
     assert Rover.at(position).position() == position
